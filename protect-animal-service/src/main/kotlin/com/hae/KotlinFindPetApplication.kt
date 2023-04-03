283d1d65392a8715ce7213f014fcf7f421351ee8
박다정 <dajung.park@hyundai-autoever.com>
2023-04-03 19:37:58 +0900

    엔티티들이 공통으로 가지는 필드를 상속받는 BaseEntity 클래스 생성 및 자동 생성일자, 수정일자 생기도록 설정

--- protect-animal-service/src/main/kotlin/com/hae/KotlinFindPetApplication.kt
+++ protect-animal-service/src/main/kotlin/com/hae/KotlinFindPetApplication.kt
@@ -2,11 +2,13 @@ package com.hae
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing //해당 어노테이션이 있어야 AuditingEntityListener가 동작
 class KotlinFindPetApplication
 
 fun main(args: Array<String>) {
-    System.setProperty("spring.config.name", "application")
+//    System.setProperty("spring.config.name", "application")
     runApplication<KotlinFindPetApplication>(*args)
 }
--- protect-animal-service/src/main/kotlin/com/hae/domain/BaseEntity.kt
+++ protect-animal-service/src/main/kotlin/com/hae/domain/BaseEntity.kt
@@ -1,4 +1,19 @@
 package com.hae.domain
 
-class BaseEntity {
-}
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass //해당 클래스가 엔티티가 아님을 나타내는 어노테이션
+@EntityListeners(AuditingEntityListener::class) //EntityListeners : 엔티티에 대한 이벤트를 수신하기 위한 리스너 클래스 등록,
+// AuditingEntityListener : @CreateDate, @LastModifiedDAte 어노테이션이 붙은 필드에 대해 엔티티가 저장되기 전 자동으로 값을 설정
+abstract class BaseEntity (
+        @CreatedDate
+        var createdAt: LocalDateTime? = null,
+
+        @LastModifiedDate
+        var modifiedAt: LocalDateTime? = null,
+)
