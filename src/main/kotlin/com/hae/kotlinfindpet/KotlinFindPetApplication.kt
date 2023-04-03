a3f72f366c4fc9c14799b6927e25cdb6fdf3a2b6
박다정 <dajung.park@hyundai-autoever.com>
2023-04-03 15:28:32 +0900

    project setting 및 디mariadb 연결

---
+++ src/main/kotlin/com/hae/kotlinfindpet/KotlinFindPetApplication.kt
@@ -0,0 +1,11 @@
+package com.hae.kotlinfindpet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinFindPetApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinFindPetApplication>(*args)
+}
---
+++ src/test/kotlin/com/hae/kotlinfindpet/KotlinFindPetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hae.kotlinfindpet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinFindPetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
